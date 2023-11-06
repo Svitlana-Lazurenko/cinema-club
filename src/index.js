8324bbdd4b169bd956a740d8cdc7da46096d3ae5
Svitlana Lazurenko <115888343+Svitlana-Lazurenko@users.noreply.github.com>
2023-11-06 17:49:30 +0200

    Change name of repository

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/cinema-club">
       <App />
     </BrowserRouter>
   </React.StrictMode>
